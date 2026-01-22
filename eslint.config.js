9cc9ce0e8a99a83afaa6a96d1b28822e46c4b129
cordis-dev <darius@adroiti.com>
2026-01-22 18:40:36 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,37 @@
 import reactHooks from "eslint-plugin-react-hooks";
 import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
 export default [
   {
-    files: ["**/*.tsx", "**/*.jsx"],
+    files: ["**/*.ts", "**/*.tsx"],
 
     languageOptions: {
       parser: tsParser,
       parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
-        ecmaFeatures: { jsx: true }
+        ecmaFeatures: {
+          jsx: true
+        }
       }
     },
 
     plugins: {
-      "react-hooks": reactHooks
+      "react-hooks": reactHooks,
+      "@typescript-eslint": tsPlugin
     },
 
     rules: {
+      // React Hooks
       "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn"
+      "react-hooks/exhaustive-deps": "warn",
+
+      // TypeScript
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { "argsIgnorePattern": "^_" }
+      ]
     }
   }
 ];
