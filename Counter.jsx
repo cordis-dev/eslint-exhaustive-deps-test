bc6ec2f74614f95015930c468d06a838709a87ad
cordis-dev <darius@adroiti.com>
2026-01-22 18:41:09 +0200

    Update Counter.jsx

--- Counter.jsx
+++ Counter.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 
-export function Counter({ step }) {
+export function Counter({ step }: { step: number }) {
   const [count, setCount] = useState(0);
+
+  const unused = 123; // ❌ @typescript-eslint/no-unused-vars
 
   useEffect(() => {
     setCount(count + step);
-  }, []); // ❌ missing dependencies: count, step
+  }, []); // ❌ react-hooks/exhaustive-deps (count, step)
 
   return <div>{count}</div>;
 }
